158231b9c367193e013f9c9bed29c7640832369d
Pedro Augusto <pedrogusferva@gmail.com>
2022-04-14 10:40:52 -0300

    fix(user) fix edit user profile photo

--- app/javascript/controllers/user_edit_controller.js
+++ app/javascript/controllers/user_edit_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ 'imgPhoto' ]
+  static targets = [ 'imgPhoto', 'photoField' ]
 
   changeImgSrc(e) {
     this.imgPhotoTarget.src = URL.createObjectURL(e.target.files[0])
